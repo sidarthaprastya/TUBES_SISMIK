4ed84c73a8733484bbdd5dd0bf5af4df64abbd51
Sidartha <sidarthatjoa@gmail.com>
2022-06-03 18:12:17 +0700

    hapus beberapa comment yang tidak perlu

--- motor_sim_lpf/src/pid_lpf/pid_lpf.ino
+++ motor_sim_lpf/src/pid_lpf/pid_lpf.ino
@@ -6,9 +6,6 @@
 #include "TimerOne.h"
 #include "EEPROM.h"
 
-// #define KP 1
-// #define KI 0
-// #define KD 0.6
 
 #define potentio_pin A0
 
@@ -58,73 +55,11 @@ void timer1Callback(){
 }
 
 
-
-//void showParsedData() {
-//    Serial.print("konstanta: ");
-//    Serial.print(kp);
-//    Serial.print(", ");
-//    Serial.print(ki);
-//    Serial.print(", ");
-//    Serial.println(kd);
-//}
-//
-//void recvWithStartEndMarkers() {
-//    static boolean recvInProgress = false;
-//    static byte ndx = 0;
-//    char startMarker = '<';
-//    char endMarker = '>';
-//    char rc;
-//
-//    while (Serial.available() > 0 && newData == false) {
-//        rc = Serial.read();
-//
-//        if (recvInProgress == true) {
-//            if (rc != endMarker) {
-//                receivedChars[ndx] = rc;
-//                ndx++;
-//                if (ndx >= numChars) {
-//                    ndx = numChars - 1;
-//                }
-//            }
-//            else {
-//                receivedChars[ndx] = '\0'; // terminate the string
-//                recvInProgress = false;
-//                ndx = 0;
-//                newData = true;
-//            }
-//        }
-//
-//        else if (rc == startMarker) {
-//            recvInProgress = true;
-//        }
-//    }
-//}
-//
-//void parseData() {      // split the data into its parts
-//
-//    char * strtokIndx; // this is used by strtok() as an index
-//
-//    strtokIndx = strtok(tempChars,",");      // get the first part - the string
-//    kp = atof(strtokIndx); // copy it to messageFromPC
-// 
-//    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-//    ki = atof(strtokIndx);     // convert this part to an integer
-//
-//    strtokIndx = strtok(NULL, ",");
-//    kd = atof(strtokIndx);     // convert this part to a float
-//
-//}
-
-
 void setup() {
   nh.initNode();
   nh.subscribe(sub_motor);
   nh.subscribe(sub_cons);
   nh.advertise(pub);
-//   pid_msg.setpoint = setpoint;
-//   pid_msg.kp = KP;
-//   pid_msg.ki = KI;
-//   pid_msg.kd = KD;
 
   Timer1.initialize(10000); // Set interrupt for 10 ms
   Timer1.attachInterrupt(timer1Callback);
@@ -137,18 +72,6 @@ void setup() {
 }
 
 void loop() {
-//  recvWithStartEndMarkers();
-//  if (newData == true) {
-//      strcpy(tempChars, receivedChars);
-//          // this temporary copy is necessary to protect the original data
-//          //   because strtok() used in parseData() replaces the commas with \0
-//      parseData();
-//      
-//      newData = false;
-//
-
-////      showParsedData();
-//  }
   
   // Load EEPROM Value
   // EEPROM in arduino takes 3.3ms
